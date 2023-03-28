0d2a2945fab8da30b9ec6283050415f9a041a863
Julian So <ch.julian.so@gmail.com>
2023-03-28 09:37:00 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './init.js';
+import './styles/main.scss';
+
+console.log('hello world test');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
